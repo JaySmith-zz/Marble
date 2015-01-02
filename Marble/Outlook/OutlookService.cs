8160ab827bde26320283655002fa43846b0fd009
JaySmith <jay@jaysmith.us>
2015-01-02 10:47:18 -0600

    Added OutlookService stub

---
+++ Marble/Outlook/OutlookService.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: smithjay
+ * Date: 1/2/2015
+ * Time: 10:46 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Marble.Outlook
+{
+	/// <summary>
+	/// Description of OutlookService.
+	/// </summary>
+	public class OutlookService
+	{
+		public OutlookService()
+		{
+		}
+	}
+}
