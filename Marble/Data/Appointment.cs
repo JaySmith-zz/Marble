cfb5b0047fa2189bbaed040a977f5a338b558fae
JaySmith <jay@jaysmith.us>
2015-03-09 13:44:36 -0500

    removed unused ctor

--- Marble/Data/Appointment.cs
+++ Marble/Data/Appointment.cs
@@ -16,10 +16,6 @@ namespace Marble.Data
 	/// </summary>
 	public class Appointment
 	{
-		public Appointment()
-		{
-		}
-		
 		public string Id { get; set; }
 		public DateTime Start { get; set; }
 		public DateTime End { get; set; }
